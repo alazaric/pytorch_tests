8828f356b5a73de49bdad53285a1ad5637e09e70
Alessandro Lazaric <lazaric@lazaric-mbp.dhcp.thefacebook.com>
2017-08-21 11:46:48 -0700

    added grid scripts

---
+++ gen_grid_hello_world.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python
+
+import argparse
+import itertools
+import json
+
+BASE_CMD = "./run_hello_world.sh"
+
+parser = argparse.ArgumentParser(description='Create a grid from json')
+parser.add_argument('json', help='Json that contains the grid')
+
+args = parser.parse_args()
+with open(args.json, 'r') as f:
+    grid = json.loads(f.read())
+
+perms = list(itertools.product(*grid.values()))
+
+for p in perms:
+    argstr = ""
+    i = 0
+    for k in grid.keys():
+        argstr += " -" + str(k) + " " + str(p[i])
+        i += 1
+    print(BASE_CMD + argstr)
--- hello_world.c
+++ hello_world.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-	int param1 = atoi(argv[1]);
-	int param2 = atoi(argv[2]);
+	int param1 = atoi(argv[2]);
+	int param2 = atoi(argv[4]);
 
 	printf("Hello World!\n");
 	printf("With parameters %d %d\n", param1, param2);
