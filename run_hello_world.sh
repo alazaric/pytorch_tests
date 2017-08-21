3a1aceae4905f0281917815315cdd164c574fc66
Alessandro Lazaric <lazaric@lazaric-mbp.dhcp.thefacebook.com>
2017-08-21 15:30:38 -0700

    scripts

--- run_hello_world.sh
+++ run_hello_world.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+## Step 1: construct the script that should be passed to crun
+
 # copy the binary files from the repository to the current machine that will run the job
-rsync /mnt/vol/gfsai-flash-east/ai-group/users/$USER/binaries/hello_world .
+exe_script=$'rsync /mnt/vol/gfsai-flash-east/ai-group/users/'$USER$'/binaries/hello_world .\n'
 
 # prepare the output file by appending the parameters
 params_with_under=$(sed -e 's/ /_/g' <<< "$@")
@@ -12,4 +14,10 @@ OUTFILE+="$params_with_under"
 OUTFILE+='.txt'
 
 # execute the job with the parameters passed to this script and store the result in a shared directory
-./hello_world $@ > $OUTFILE
+exe_script+="./hello_world $@ > "$OUTFILE
+
+# let us display what we are going to pass to crun
+echo "$exe_script"
+
+## Step 2: actually execute the desired command on the cluster through crun
+echo "$exe_script" | crun -G 1
