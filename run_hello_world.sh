6896d602663c5dcbd3e351448c65c7322bb30aa7
Alessandro <lazaric@fb.com>
2017-08-21 10:39:31 -0700

    Preparing scripts

--- run_hello_world.sh
+++ run_hello_world.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# copy the binary files from the repository to the current machine that will run the job
 rsync /mnt/vol/gfsai-flash-east/ai-group/users/$USER/binaries/hello_world .
-./hello_world > /mnt/vol/gfsai-flash-east/ai-group/users/$USER/results/res_hello_world.txt
+
+tmp=$(sed -e 's/ /_/g' <<< "$@")
+echo "$tmp" 
+
+OUTFILE='/mnt/vol/gfsai-flash-east/ai-group/users/$USER/results/res_hello_world'
+OUTFILE+=$@
+OUTFILE+='.txt'
+#echo $OUTFILE
+# execute the job with the parameters passed to this script and store the result in a shared directory
+#./hello_world $@ > $OUTFILE
