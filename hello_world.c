59a45e9797165130f4e8508f2933eb2f6c576af5
Alessandro Lazaric <lazaric@lazaric-mbp.dhcp.thefacebook.com>
2017-08-21 09:52:48 -0700

    Added parameters

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+
+	int param1 = atoi(argv[1]);
+	int param2 = atoi(argv[2]);
+
 	printf("Hello World!\n");
+	printf("With parameters %d %d\n", param1, param2);
 	return 0;
 }
